045569d5e06426e5dff0adbbe6c2c23c585dca52
John Rhoads <john_rhoads@acm.org>
2023-10-02 12:37:52 -0400

    added shebang for bash and explanatory comment in runit.sh

--- runit.sh
+++ runit.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+# runs gradle on core build and captures sysout and syserr output in file with timestamp in front
 DATEFILE='%Y-%m-%d-%H%m'
 FILENAME=$(date +$DATEFILE)-build.log
 echo $FILENAME
